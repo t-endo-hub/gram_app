a4410148274dd485fdd6fb87aa3b206c612ce79f
Takumi Endo <endotaku0608@gmail.com>
2020-07-26 02:16:11 +0000

    finish like

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :name, presence: true, length: { maximum: 50}
   has_many :posts, dependent: :destroy
+  has_many :likes
 
   def update_without_current_password(params, *options)
     params.delete(:current_password)
